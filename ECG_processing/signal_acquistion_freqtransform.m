81198f5293476b86e7f68975b98a0c16873c6fb5
Suyash Subodh Shirsat <suyashshirsat8@gmail.com>
2026-09-20 00:28:49 +0530

    Add files via upload

---
+++ ECG_processing/signal_acquistion_freqtransform.m
@@ -0,0 +1,17 @@
+﻿clc all;
+close all;
+clear all;
+
+ecg_data=csvread("synthetic_ecg_2000.csv");
+Fs=250;
+f=ecg_data(:,3);
+figure(1),plot(f);
+
+F=fft(f);
+Fabs=abs(F);
+
+len=length(f);
+
+freq = Fs*(0:(len-1))/len;
+
+figure(2),plot(freq,Fabs);
